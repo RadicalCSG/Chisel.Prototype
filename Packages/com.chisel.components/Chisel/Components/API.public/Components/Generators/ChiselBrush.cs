b35ca44625aaca231029e3970adc8895a572847a
Sander van Rossen <sander.vanrossen@gmail.com>
2019-07-19 09:24:09 +0200

    Added BrushDefinition to ChiselBrush

--- Packages/com.chisel.components/Chisel/Components/API.public/Components/Generators/ChiselBrush.cs
+++ Packages/com.chisel.components/Chisel/Components/API.public/Components/Generators/ChiselBrush.cs
@@ -6,16 +6,9 @@ namespace Chisel.Components
     [ExecuteInEditMode]
     [HelpURL(kDocumentationBaseURL + kNodeTypeName + kDocumentationExtension)]
     [AddComponentMenu("Chisel/" + kNodeTypeName)]
-    public sealed class ChiselBrush : ChiselGeneratorComponent
+    public sealed class ChiselBrush : ChiselDefinedGeneratorComponent<BrushDefinition>
     {
         public const string kNodeTypeName = "Brush";
         public override string NodeTypeName { get { return kNodeTypeName; } }
-
-        public override void UpdateGenerator()
-        {
-            // This class doesn't generate anything, so we keep the original brush
-        }
-
-        protected override void UpdateGeneratorInternal() { }
     }
 }
