7f87610bc29bae0ffc840964071d529cbb1d3899
Daniel Cornelius <Kerfuffles@users.noreply.github.com>
2020-10-21 18:48:03 -0500

    clean up some per-frame allocations

--- Packages/com.chisel.editor/Chisel/Editor/Editor/MaterialBrowser/Testing/ChiselMaterialBrowserWindowLayoutTest.cs
+++ Packages/com.chisel.editor/Chisel/Editor/Editor/MaterialBrowser/Testing/ChiselMaterialBrowserWindowLayoutTest.cs
@@ -24,8 +24,8 @@ namespace Chisel.Editors
         private        List<ChiselMaterialBrowserTile> m_Tiles  = new List<ChiselMaterialBrowserTile>();
         private        List<string>                    m_Labels = new List<string>();
 
-        private int m_LastSelectedMaterial = 0;
-        private int m_CurrentPropsTab      = 1;
+        private int m_LastSelectedMaterialIndex = 0;
+        private int m_CurrentPropsTab           = 1;
 
         private int     m_TileSize  = 64;
         private Vector2 m_ScrollPos = Vector2.zero;
@@ -92,7 +92,7 @@ namespace Chisel.Editors
             } );
 
             // label bar
-            m_TopToolBar2        = Root.AddToolbar( "toolBar" );
+            m_TopToolBar2       = Root.AddToolbar( "toolBar" );
             m_SearchFilterLabel = m_TopToolBar2.AddLabel( "", "statusBarLabel" );
             m_SearchFilterLabel.SetSize( 200, 24 );
             m_TopToolBar2Spacer0 = m_TopToolBar2.AddSpacer( "topToolBar2Spacer0" );
@@ -121,10 +121,18 @@ namespace Chisel.Editors
 
         private Material m_PreviewMaterial;
         private Editor   m_PreviewEditor;
+        private Rect     m_PropsAreaRect = Rect.zero;
+        private Rect     m_NotifRect     = Rect.zero;
 
         private void OnGUI()
         {
-            int lastSelected = m_LastSelectedMaterial;
+            int lastSelected = m_LastSelectedMaterialIndex;
+
+            m_NotifRect.x      = ( position.width  * 0.5f ) - 200;
+            m_NotifRect.y      = ( position.height * 0.5f ) - 60;
+            m_NotifRect.width  = 400;
+            m_NotifRect.height = 120;
+
             // rebuild IMGUI styling
             RebuildStyles();
 
@@ -135,111 +143,121 @@ namespace Chisel.Editors
             if( EditorApplication.isCompiling )
             {
                 Root.visible = false;
-                GUI.Box( new Rect( ( position.width * 0.5f ) - 200, ( position.height * 0.5f ) - 60, 400, 120 ), "Please wait...", "NotificationBackground" );
+                GUI.Box( m_NotifRect, "Please wait...", "NotificationBackground" );
             }
             else Root.visible = true;
 
             if( !EditorApplication.isCompiling && !EditorApplication.isPlaying )
             {
-                GUI.Box( new Rect( position.width - 264, 48, 240, position.height - 48 ), "" );
+                //GUI.Box( new Rect( position.width - 264, 48, 240, position.height - 48 ), "" );
 
                 switch( m_CurrentPropsTab )
                 {
                     case 0:
                     {
-                        Rect labelButtonRect = new Rect( position.width - 260, 50, 224, 24 );
+                        m_PropsAreaRect.x      = position.width - 260;
+                        m_PropsAreaRect.y      = 50;
+                        m_PropsAreaRect.width  = 224;
+                        m_PropsAreaRect.height = 24;
 
                         for( int i = 0; i < m_Labels.Count; i++ )
                         {
                             if( i > 0 )
-                                labelButtonRect.y += 26;
-                            if( GUI.Button( labelButtonRect, m_Labels[i] ) ) {}
+                                m_PropsAreaRect.y += 26;
+                            if( GUI.Button( m_PropsAreaRect, m_Labels[i] ) ) {}
                         }
 
                         break;
                     }
                     case 1:
                     {
-                        Rect propsAreaRect = new Rect( position.width - 264, 50, 240, position.height - 50 );
+                        m_PropsAreaRect.x      = position.width - 264;
+                        m_PropsAreaRect.y      = 50;
+                        m_PropsAreaRect.width  = 240;
+                        m_PropsAreaRect.height = position.height - 50;
 
-                        GUI.BeginGroup( propsAreaRect, (GUIStyle) "gameviewbackground" );
+                        GUI.BeginGroup( m_PropsAreaRect, (GUIStyle) "gameviewbackground" );
                         {
-                            Rect contentPos = new Rect( 2, 2, 234, 234 );
+                            m_PropsAreaRect.x      = 2;
+                            m_PropsAreaRect.y      = 2;
+                            m_PropsAreaRect.width  = 234;
+                            m_PropsAreaRect.height = 234;
 
-                            if( m_Tiles[m_LastSelectedMaterial] != null )
+                            if( m_Tiles[m_LastSelectedMaterialIndex] != null && m_PreviewMaterial != null )
                             {
                                 if( m_PreviewEditor == null ) m_PreviewEditor = Editor.CreateEditor( m_PreviewMaterial, typeof( MaterialEditor ) );
-                                m_PreviewEditor.OnInteractivePreviewGUI( contentPos, EditorStyles.whiteLabel );
+                                m_PreviewEditor.OnInteractivePreviewGUI( m_PropsAreaRect, EditorStyles.whiteLabel );
                                 m_PreviewEditor.Repaint();
                             }
-                            //GUI.DrawTexture( contentPos, m_Tiles[m_LastSelectedMaterial].Preview );
+                            //GUI.DrawTexture( contentPos, m_Tiles[m_LastSelectedMaterialIndex].Preview );
 
-                            propsAreaRect.y      = 236;
-                            propsAreaRect.x      = 1;
-                            propsAreaRect.height = 90;
-                            propsAreaRect.width  = 237;
-                            GUI.BeginGroup( propsAreaRect, (GUIStyle) "helpbox" );
+                            m_PropsAreaRect.y      = 236;
+                            m_PropsAreaRect.x      = 1;
+                            m_PropsAreaRect.height = 90;
+                            m_PropsAreaRect.width  = 237;
+
+                            GUI.BeginGroup( m_PropsAreaRect, (GUIStyle) "helpbox" );
                             {
-                                propsAreaRect.x      += 1;
-                                propsAreaRect.y      =  0;
-                                propsAreaRect.height =  22;
-                                GUI.Label( propsAreaRect, $"{m_Tiles[m_LastSelectedMaterial].materialName}", EditorStyles.toolbarButton );
-                                propsAreaRect.y += 22;
-                                GUI.Label( propsAreaRect, $"Size:\t{m_Tiles[m_LastSelectedMaterial].mainTexSize:###0}", EditorStyles.miniLabel );
-                                propsAreaRect.y += 22;
-                                GUI.Label( propsAreaRect, $"Offset:\t{m_Tiles[m_LastSelectedMaterial].uvOffset:####0}", EditorStyles.miniLabel );
-                                propsAreaRect.y += 22;
-                                GUI.Label( propsAreaRect, $"Scale:\t{m_Tiles[m_LastSelectedMaterial].uvScale:####0}", EditorStyles.miniLabel );
+                                m_PropsAreaRect.x      += 1;
+                                m_PropsAreaRect.y      =  0;
+                                m_PropsAreaRect.height =  22;
+                                GUI.Label( m_PropsAreaRect, $"{m_Tiles[m_LastSelectedMaterialIndex].materialName}", EditorStyles.toolbarButton );
+                                m_PropsAreaRect.y += 22;
+                                GUI.Label( m_PropsAreaRect, $"Size:\t{m_Tiles[m_LastSelectedMaterialIndex].mainTexSize:###0}", EditorStyles.miniLabel );
+                                m_PropsAreaRect.y += 22;
+                                GUI.Label( m_PropsAreaRect, $"Offset:\t{m_Tiles[m_LastSelectedMaterialIndex].uvOffset:####0}", EditorStyles.miniLabel );
+                                m_PropsAreaRect.y += 22;
+                                GUI.Label( m_PropsAreaRect, $"Scale:\t{m_Tiles[m_LastSelectedMaterialIndex].uvScale:####0}", EditorStyles.miniLabel );
                             }
                             GUI.EndGroup();
 
-                            propsAreaRect.y      += 262;
-                            propsAreaRect.height =  140;
-                            GUI.BeginGroup( propsAreaRect, (GUIStyle) "helpbox" );
+                            m_PropsAreaRect.y      += 262;
+                            m_PropsAreaRect.height =  140;
+                            GUI.BeginGroup( m_PropsAreaRect, (GUIStyle) "helpbox" );
                             {
-                                propsAreaRect.y      = 0;
-                                propsAreaRect.x      = 0;
-                                propsAreaRect.height = 22;
-                                GUI.Label( propsAreaRect, $"Material", EditorStyles.toolbarButton );
-                                propsAreaRect.x += 1;
-                                propsAreaRect.y += 22;
-                                GUI.Label( propsAreaRect, $"Shader:\t{m_Tiles[m_LastSelectedMaterial].shaderName}", EditorStyles.miniLabel );
-                                propsAreaRect.y += 22;
-                                GUI.Label( propsAreaRect, $"Albedo:\t{m_Tiles[m_LastSelectedMaterial].albedoName}", EditorStyles.miniLabel );
+                                m_PropsAreaRect.y      = 0;
+                                m_PropsAreaRect.x      = 0;
+                                m_PropsAreaRect.height = 22;
+                                GUI.Label( m_PropsAreaRect, $"Material", EditorStyles.toolbarButton );
+                                m_PropsAreaRect.x += 1;
+                                m_PropsAreaRect.y += 22;
+                                GUI.Label( m_PropsAreaRect, $"Shader:\t{m_Tiles[m_LastSelectedMaterialIndex].shaderName}", EditorStyles.miniLabel );
+                                m_PropsAreaRect.y += 22;
+                                GUI.Label( m_PropsAreaRect, $"Albedo:\t{m_Tiles[m_LastSelectedMaterialIndex].albedoName}", EditorStyles.miniLabel );
 
-                                propsAreaRect.x     += 1;
-                                propsAreaRect.y     =  88;
-                                propsAreaRect.width -= 4;
-                                if( GUI.Button( propsAreaRect, "Select In Project", "largebutton" ) )
+                                m_PropsAreaRect.x     += 1;
+                                m_PropsAreaRect.y     =  88;
+                                m_PropsAreaRect.width -= 4;
+                                if( GUI.Button( m_PropsAreaRect, "Select In Project", "largebutton" ) )
                                 {
-                                    Material m = AssetDatabase.LoadAssetAtPath<Material>( AssetDatabase.GUIDToAssetPath( m_Tiles[m_LastSelectedMaterial].guid ) );
+                                    Material m = AssetDatabase.LoadAssetAtPath<Material>( AssetDatabase.GUIDToAssetPath( m_Tiles[m_LastSelectedMaterialIndex].guid ) );
                                     Selection.activeObject = m;
                                     EditorGUIUtility.PingObject( m );
                                 }
 
-                                propsAreaRect.y += 24;
-                                if( GUI.Button( propsAreaRect, "Apply to Selected Face", "largebutton" ) ) {}
+                                m_PropsAreaRect.y += 24;
+                                if( GUI.Button( m_PropsAreaRect, "Apply to Selected Face", "largebutton" ) ) {}
                             }
                             GUI.EndGroup();
 
-                            propsAreaRect.y      = 469;
-                            propsAreaRect.height = 186;
-                            GUI.BeginGroup( propsAreaRect, (GUIStyle) "helpbox" );
+                            m_PropsAreaRect.y      = 469;
+                            m_PropsAreaRect.height = 186;
+                            GUI.BeginGroup( m_PropsAreaRect, (GUIStyle) "helpbox" );
                             {
-                                propsAreaRect.y      = 0;
-                                propsAreaRect.height = 22;
-                                GUI.Label( propsAreaRect, "Labels", EditorStyles.toolbarButton );
+                                m_PropsAreaRect.y      = 0;
+                                m_PropsAreaRect.height = 22;
+                                GUI.Label( m_PropsAreaRect, "Labels", EditorStyles.toolbarButton );
 
                                 float btnWidth = 60;
                                 int   idx      = 0;
-                                foreach( var l in m_Tiles[m_LastSelectedMaterial].labels )
+                                foreach( var l in m_Tiles[m_LastSelectedMaterialIndex].labels )
                                 {
                                     float xOffset = 2;
                                     float yOffset = 22;
                                     int   row     = 0;
-                                    for( int i = 0; i < ( propsAreaRect.width / btnWidth ); i++ )
+                                    for( int i = 0; i < ( m_PropsAreaRect.width / btnWidth ); i++ )
                                     {
-                                        if( idx == m_Tiles[m_LastSelectedMaterial].labels.Length ) break;
+                                        if( idx == m_Tiles[m_LastSelectedMaterialIndex].labels.Length ) break;
 
                                         xOffset = ( 40 * i ) + 2;
                                         if( GUI.Button( new Rect( xOffset, yOffset, btnWidth, 22 ), new GUIContent( l, $"Click to filter for label \"{l}\"" ), m_AssetLabelStyle ) )
@@ -268,19 +286,29 @@ namespace Chisel.Editors
             }
         }
 
-        // legacy code
-        private float scrollViewHeight;
+        private float      m_ScrollViewHeight;
+        private Rect       m_TileContentRect = Rect.zero;
+        private Rect       m_ScrollViewRect  = Rect.zero;
+        private GUIContent m_TileContentText = new GUIContent();
 
         private void DrawLabelAndTileArea()
         {
-            Rect rect = position;
             //GUI.Box( new Rect( 0, 48, rect.width - 24, rect.height - ( 48 * 2 ) ), "", "GameViewBackground" );
 
             int idx        = 0;
-            int numColumns = (int) ( ( rect.width - ( 264 + 10 ) ) / m_TileSize );
+            int numColumns = (int) ( ( position.width - ( 264 + 10 ) ) / m_TileSize );
 
-            m_ScrollPos = GUI.BeginScrollView( new Rect( 0, 52, rect.width - 262, rect.height - 72 ), m_ScrollPos,
-                                               new Rect( 0, 0,  rect.width - 282, scrollViewHeight ) );
+            m_ScrollViewRect.x      = 0;
+            m_ScrollViewRect.y      = 52;
+            m_ScrollViewRect.width  = position.width  - 262;
+            m_ScrollViewRect.height = position.height - 72;
+
+            m_TileContentRect.x      = 0;
+            m_TileContentRect.y      = 0;
+            m_TileContentRect.width  = position.width - 282;
+            m_TileContentRect.height = m_ScrollViewHeight;
+
+            m_ScrollPos = GUI.BeginScrollView( m_ScrollViewRect, m_ScrollPos, m_TileContentRect );
             {
                 float xOffset = 0;
                 float yOffset = 0;
@@ -297,31 +325,50 @@ namespace Chisel.Editors
 
                         if( idx == m_Tiles.Count ) break;
 
-                        if( GUI.Button( new Rect( xOffset, yOffset, m_TileSize - 2, m_TileSize - 4 ),
-                                        new GUIContent( m_Tiles[idx].Preview, $"Name: {m_Tiles[idx].materialName}\nShader: {m_Tiles[idx].shaderName}" ),
-                                        m_TileButtonStyle ) ) // make a custom guistyle for these, or make a method to do a proper tile
+                        m_TileContentRect.x      = xOffset;
+                        m_TileContentRect.y      = yOffset;
+                        m_TileContentRect.width  = m_TileSize - 2;
+                        m_TileContentRect.height = m_TileSize - 4;
+
+                        m_TileContentText.image   = m_Tiles[idx].Preview;
+                        m_TileContentText.tooltip = $"Name: {m_Tiles[idx].materialName}\nShader: {m_Tiles[idx].shaderName}";
+
+                        if( GUI.Button( m_TileContentRect, m_TileContentText, m_TileButtonStyle ) )
                         {
                             m_PreviewMaterial = AssetDatabase.LoadAssetAtPath<Material>( AssetDatabase.GUIDToAssetPath( m_Tiles[idx].guid ) );
                             m_PreviewEditor   = null;
 
-                            m_LastSelectedMaterial = idx;
+                            m_LastSelectedMaterialIndex = idx;
                         }
 
                         idx++;
                     }
 
-                    row              += 1;
-                    yOffset          =  ( m_TileSize * row );
-                    scrollViewHeight =  yOffset;
+                    row                += 1;
+                    yOffset            =  ( m_TileSize * row );
+                    m_ScrollViewHeight =  yOffset;
 
                     // end horizontal
                 }
             }
             GUI.EndScrollView();
 
-            GUI.Box( new Rect( 0,   rect.height - 20, rect.width, 20 ), "", "toolbar" );
-            GUI.Label( new Rect( 0, rect.height - 20, 400,        20 ), $"Materials: {m_Tiles.Count} | Labels: {m_Labels.Count}" );
-            m_TileSize = (int) GUI.HorizontalSlider( new Rect( rect.width - 360, rect.height - 20, 80, 24 ), m_TileSize, 64, 128 );
+            m_TileContentRect.x      = 0;
+            m_TileContentRect.y      = position.height - 20;
+            m_TileContentRect.width  = position.width;
+            m_TileContentRect.height = 20;
+
+            GUI.Box( m_TileContentRect, "", "toolbar" );
+
+            m_TileContentRect.width = 400;
+            GUI.Label( m_TileContentRect, $"Materials: {m_Tiles.Count} | Labels: {m_Labels.Count}" );
+
+            m_TileContentRect.x      = position.width  - 360;
+            m_TileContentRect.y      = position.height - 20;
+            m_TileContentRect.width  = 80;
+            m_TileContentRect.height = 24;
+
+            m_TileSize = (int) GUI.HorizontalSlider( m_TileContentRect, m_TileSize, 64, 128 );
         }
     }
 }
