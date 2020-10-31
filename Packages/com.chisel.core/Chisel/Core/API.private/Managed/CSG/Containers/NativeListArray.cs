8d29046a55800a4143d2b1d27b5fc7b856acfc07
Sander van Rossen <sander@unity3d.com>
2020-10-31 13:17:47 +0100

    updated packages

--- Packages/com.chisel.core/Chisel/Core/API.private/Managed/CSG/Containers/NativeListArray.cs
+++ Packages/com.chisel.core/Chisel/Core/API.private/Managed/CSG/Containers/NativeListArray.cs
@@ -350,7 +350,7 @@ namespace Chisel.Core
             CheckArgPositive(initialListCapacity);
 
 
-            CollectionHelper.CheckIsUnmanaged<T>();
+            //CollectionHelper.CheckIsUnmanaged<T>();
             CheckArgInRange(totalSize, int.MaxValue);
 
             DisposeSentinel.Create(out m_Safety, out m_DisposeSentinel, disposeSentinelStackDepth, allocator);
